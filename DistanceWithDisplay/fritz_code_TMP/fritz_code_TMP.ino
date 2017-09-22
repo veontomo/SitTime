3c50a1882712736b0e02b2b5b4ad5a5be1109ebf
Andrey Shcherbakov <veontomo@gmail.com>
2017-09-22 23:17:57 +0200

    Add the Fritzing sketch

--- DistanceWithDisplay/fritz_code_TMP/fritz_code_TMP.ino
+++ DistanceWithDisplay/fritz_code_TMP/fritz_code_TMP.ino
@@ -1,52 +1,3 @@
-// push button: https://www.arduino.cc/en/Tutorial/Button
-// lcd: https://www.arduino.cc/en/Tutorial/HelloWorld
-// autoscroll for lcd: https://www.arduino.cc/en/Tutorial/LiquidCrystalAutoscroll
-// distance measurement: https://create.arduino.cc/projecthub/silicon-ripley-10/distance-measurement-using-ultrasonic-sensor-and-arduino-9bacd4
-
-
-/*
-  LiquidCrystal Library - Hello World
-
- Demonstrates the use a 16x2 LCD display.  The LiquidCrystal
- library works with all LCD displays that are compatible with the
- Hitachi HD44780 driver. There are many of them out there, and you
- can usually tell them by the 16-pin interface.
-
- This sketch prints "Hello World!" to the LCD
- and shows the time.
-
-  The circuit:
- * LCD RS pin to digital pin 12
- * LCD Enable pin to digital pin 11
- * LCD D4 pin to digital pin 5
- * LCD D5 pin to digital pin 4
- * LCD D6 pin to digital pin 3
- * LCD D7 pin to digital pin 2
- * LCD R/W pin to ground
- * LCD VSS pin to ground
- * LCD VCC pin to 5V
- * 10K resistor:
- * ends to +5V and ground
- * wiper to LCD VO pin (pin 3)
-
- Library originally added 18 Apr 2008
- by David A. Mellis
- library modified 5 Jul 2009
- by Limor Fried (http://www.ladyada.net)
- example added 9 Jul 2009
- by Tom Igoe
- modified 22 Nov 2010
- by Tom Igoe
- modified 7 Nov 2016
- by Arturo Guadalupi
-
- This example code is in the public domain.
-
- http://www.arduino.cc/en/Tutorial/LiquidCrystalHelloWorld
-
-*/
-
-// include the library code:
 #include <LiquidCrystal.h>
 
 const int trigPin = 9;
@@ -169,5 +120,4 @@ void printPercent(int col, int row, int value){
     lcd.print("%");
   }
 }
-
 
